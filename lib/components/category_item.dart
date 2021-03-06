267178353bac005bb976c5e8245fad15c7c0635d
Yuumi <leovilela.empresa@gmail.com>
2021-03-05 23:54:22 -0400

    add routes

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:food/models/category.dart';
+import 'package:food/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Center(
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Center(
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
         ),
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
